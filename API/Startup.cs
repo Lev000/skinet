a24e5e3c579f708dd1278549e415b9001f2bb8a1
Lev <levchifman@gmail.com>
2020-09-02 14:54:16 -0400

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,8 +12,11 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 
+using AutoMapper;
+
 using Infrastructure.Data;
 using Core.Interfaces;
+using API.Helpers;
 
 namespace API
 {
@@ -33,6 +36,8 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x=>x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+            services.AddAutoMapper(typeof(MappingProfiles));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +51,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseStaticFiles();
 
             app.UseAuthorization();
 
---
+++ Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,48 @@
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+using Core.Interfaces;
+using Core.Entities;
+using Core.Specifications;
+
+namespace Infrastructure.Data
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly StoreContext _context;
+        public GenericRepository(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        
+
+        public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+        {
+            return await applySpecification(spec).FirstOrDefaultAsync();
+
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync(ISpecification<T> spec)
+        {
+            return await applySpecification(spec).ToListAsync();
+        }
+
+        private IQueryable<T> applySpecification(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
+        }
+    }
+}
